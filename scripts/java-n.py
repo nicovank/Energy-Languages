f43e146f970cf1e39433e1d22a51fcf559a0c9c2
Nicolas van Kempen <nvankempen@meta.com>
2023-10-12 14:58:59 +0100

    Java-N script bug fixes

--- scripts/java-n.py
+++ scripts/java-n.py
@@ -22,28 +22,31 @@ def main(args):
             for line in f:
                 data[n].append(json.loads(line))
 
-        x = np.sort(np.array(list(data.keys())))
-        runtime = [[r["runtime"] for r in data[n]] for n in x]
-        energy = [[r["energy"]["pkg"] for r in data[n]] for n in x]
+    plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
+    with plt.style.context("bmh"):
+        fig, ax = plt.subplots()
+        fig.set_size_inches(7, 5)
 
-        y = np.median(runtime, axis=1) / x
-        sigma = np.std(runtime, axis=1) / x
+        for n in sorted(data.keys()):
+            x = np.sort(np.array(list(data.keys())))
+            runtime = [[r["runtime"] for r in data[n]] for n in x]
+            energy = [[r["energy"]["pkg"] for r in data[n]] for n in x]
 
-        plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
-        with plt.style.context("bmh"):
-            fig, ax = plt.subplots()
-            fig.set_size_inches(10, 7)
-            if not args.no_title:
-                ax.set_title(f"Runtime per iteration for {args.benchmark} (Java)")
+            y = np.median(runtime, axis=1) / x
+            sigma = np.std(runtime, axis=1) / x
 
             ax.scatter(x, y, marker=".")
             ax.errorbar(x, y, sigma, linestyle="", elinewidth=1, capsize=2, alpha=0.5)
-            ax.set_axisbelow(True)
-            ax.set_ylim(0, ax.get_ylim()[1])
-            ax.set_ylabel("Time [ms]")
-            ax.set_xlabel("Number of iterations")
 
-            plt.savefig(f"java-n.{args.benchmark}.{args.format}", format=args.format)
+        if not args.no_title:
+            ax.set_title(f"Runtime per iteration for {args.benchmark} (Java)")
+        ax.set_ylabel("Time [ms]")
+        ax.set_xlabel("Number of iterations")
+        ax.set_axisbelow(True)
+        ax.set_ylim(0, ax.get_ylim()[1])
+
+        fig.tight_layout()
+        plt.savefig(f"java-n.{args.benchmark}.{args.format}", format=args.format)
 
 
 if __name__ == "__main__":
