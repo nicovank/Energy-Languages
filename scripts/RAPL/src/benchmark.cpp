31b3a414ce414951e22ad7b8e0b562144405bcdc
Nicolas van Kempen <nvankemp@gmail.com>
2024-03-29 23:53:31 +0000

    Add perf event colection

--- scripts/RAPL/src/benchmark.cpp
+++ scripts/RAPL/src/benchmark.cpp
@@ -1,8 +1,16 @@
 #include <chrono>
 #include <cstdint>
+#include <cstring>
 #include <iostream>
+#include <memory>
 #include <thread>
 #include <vector>
+
+#include <linux/hw_breakpoint.h>
+#include <linux/perf_event.h>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+#include <unistd.h>
 
 #include <argparse/argparse.hpp>
 #include <benchmark/benchmark.h>
@@ -13,6 +21,8 @@
 #error "This tool requires MSR PKG domain support"
 #endif
 
+static std::unique_ptr<std::vector<std::uint8_t>> memory;
+
 void work(std::chrono::seconds duration) {
     auto start = std::chrono::high_resolution_clock::now();
     while (std::chrono::high_resolution_clock::now() - start < duration) {
@@ -21,6 +31,108 @@ void work(std::chrono::seconds duration) {
         }
     }
 }
+
+namespace perf {
+int perf_event_open(struct perf_event_attr* attr, pid_t pid, int cpu, int group_fd, unsigned long flags) {
+    return syscall(__NR_perf_event_open, attr, pid, cpu, group_fd, flags);
+}
+
+// Cache event only, TODO later.
+struct Event {
+    explicit Event(std::uint64_t config) {
+        struct perf_event_attr attr;
+        std::memset(&attr, 0, sizeof(struct perf_event_attr));
+        attr.type = PERF_TYPE_HW_CACHE;
+        attr.size = sizeof(struct perf_event_attr);
+        attr.config = config;
+        attr.disabled = 1;
+        attr.inherit = 1;
+
+        fd = perf_event_open(&attr, 0, -1, -1, 0);
+        if (fd == -1) {
+            std::cerr << "[ERROR] Failed to open perf event: " << strerror(errno) << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+        assert(fd != -1);
+    }
+
+    ~Event() {
+        close(fd);
+    }
+
+    Event(const Event&) = delete;
+    Event(Event&&) = default;
+    Event& operator=(const Event&) = delete;
+    Event& operator=(Event&&) = default;
+
+    void reset() {
+        const auto status = ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+        assert(status == 0);
+    }
+
+    void enable() {
+        const auto status = ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+        assert(status == 0);
+    }
+
+    void disable() {
+        const auto status = ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+        assert(status == 0);
+    }
+
+    std::uint64_t read() const {
+        std::uint64_t count;
+        const auto status = ::read(fd, &count, sizeof(std::uint64_t));
+        assert(status == sizeof(std::uint64_t));
+        return count;
+    }
+
+  private:
+    int fd;
+};
+
+// Would love to use PERF_FORMAT_GROUP but it messes with inherit.
+struct EventGroup {
+    // EventGroup(std::vector<Event> events) : events(std::move(events)) {}
+
+    EventGroup(std::vector<std::uint64_t> configs) {
+        events.reserve(configs.size());
+        for (const auto& config : configs) {
+            events.emplace_back(config);
+        }
+    }
+
+    void reset() {
+        for (auto& event : events) {
+            event.reset();
+        }
+    }
+
+    void enable() {
+        for (auto& event : events) {
+            event.enable();
+        }
+    }
+
+    void disable() {
+        for (auto& event : events) {
+            event.disable();
+        }
+    }
+
+    std::vector<std::uint64_t> read() const {
+        std::vector<std::uint64_t> counts;
+        counts.reserve(events.size());
+        for (const auto& event : events) {
+            counts.push_back(event.read());
+        }
+        return counts;
+    }
+
+  private:
+    std::vector<Event> events;
+};
+} // namespace perf
 
 int main(int argc, char** argv) {
     auto program = argparse::ArgumentParser("benchmark", "", argparse::default_arguments::help);
@@ -37,6 +149,12 @@ int main(int argc, char** argv) {
         .metavar("SECONDS")
         .scan<'d', std::uint64_t>();
 
+    program.add_argument("--percentage-misses")
+        .required()
+        .help("approximate LLC cache miss percentage")
+        .metavar("SECONDS")
+        .scan<'g', double>();
+
     try {
         program.parse_args(argc, argv);
     } catch (const std::exception& err) {
@@ -47,11 +165,32 @@ int main(int argc, char** argv) {
 
     auto cores = program.get<std::uint8_t>("cores");
     auto duration = program.get<std::uint64_t>("duration");
+    auto misses = program.get<double>("percentage-misses");
 
     if (cores == 0) {
-        std::cerr << "[ERROR] Can't ran a benchmark on zero cores!" << std::endl;
+        std::cerr << "[ERROR] Can't run a benchmark on zero cores." << std::endl;
         return EXIT_FAILURE;
     }
+
+    if (misses < 0 || misses > 1) {
+        std::cerr << "[ERROR] Invalid cache miss percentage." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    auto group = perf::EventGroup(
+        {PERF_COUNT_HW_CACHE_LL | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
+         PERF_COUNT_HW_CACHE_LL | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16),
+         PERF_COUNT_HW_CACHE_LL | (PERF_COUNT_HW_CACHE_OP_WRITE << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
+         PERF_COUNT_HW_CACHE_LL | (PERF_COUNT_HW_CACHE_OP_WRITE << 8) | (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16)});
+
+    // Note: This should be greater than the L3 cache size
+    // memory = std::make_unique<std::vector<std::uint8_t>>(1'000'000'000);
+    // for (int i = 0; i < 1'000'000'000; ++i) {
+    //     memory->at(i) = i;
+    // }
+
+    group.reset();
+    group.enable();
 
     if (cores == 1) {
         work(std::chrono::seconds(duration));
@@ -65,5 +204,14 @@ int main(int argc, char** argv) {
         }
     }
 
-    std::cout << "Time: " << duration << " seconds" << std::endl;
+    group.disable();
+    const auto counts = group.read();
+    std::cout << "llc-read-misses    : " << counts[0] << std::endl;
+    std::cout << "llc-read-accesses  : " << counts[1] << std::endl;
+    std::cout << "llc-write-misses   : " << counts[2] << std::endl;
+    std::cout << "llc-write-accesses : " << counts[3] << std::endl;
+    std::cout << "llc-read-miss-rate : " << 100 * static_cast<double>(counts[0]) / static_cast<double>(counts[1])
+              << std::endl;
+    std::cout << "llc-write-miss-rate: " << 100 * static_cast<double>(counts[2]) / static_cast<double>(counts[3])
+              << std::endl;
 }
