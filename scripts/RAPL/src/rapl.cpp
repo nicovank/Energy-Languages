37956362ce2e4b58a65c3eb744ee49707df236cd
Nicolas van Kempen <nvankemp@gmail.com>
2024-04-04 01:11:42 +0000

    C++

--- scripts/RAPL/src/rapl.cpp
+++ scripts/RAPL/src/rapl.cpp
@@ -15,7 +15,7 @@ double getEnergyUnitForPackage(int package) {
         for (int i = 0; i < cpu::getNPackages(); ++i) {
             const auto fd = msr::open(cpu::getLowestNumberedCpuForPackage(i));
             const auto result = msr::read(fd, MSR_RAPL_POWER_UNIT);
-            units.push_back(pow(0.5, (double) ((result >> 8) & 0x1f)));
+            units.push_back(std::pow(0.5, (double) ((result >> 8) & 0x1f)));
             close(fd);
         }
         return units;
