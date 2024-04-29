68b1bbac7d0b32842a639c28fbd5139b9fb2461d
Nicolas van Kempen <nvankemp@gmail.com>
2024-04-29 16:58:44 +0000

    Move rusage things to separate file

---
+++ scripts/RAPL/include/rapl/rusage.hpp
@@ -0,0 +1,41 @@
+#include <sys/resource.h>
+#include <sys/time.h>
+
+#include <glaze/core/common.hpp>
+
+struct timeval& operator-=(struct timeval& left, const struct timeval& right);
+struct timeval operator-(struct timeval left, const struct timeval& right);
+
+struct rusage& operator-=(struct rusage& left, const struct rusage& right);
+struct rusage operator-(struct rusage left, const struct rusage& right);
+
+template <>
+struct glz::meta<struct timeval> {
+    using T = struct timeval;
+    [[maybe_unused]] static constexpr auto value = glz::object("tv_sec", &T::tv_sec, "tv_usec", &T::tv_usec);
+};
+
+template <>
+struct glz::meta<struct rusage> {
+    using T = struct rusage;
+    // clang-format off
+    [[maybe_unused]] static constexpr auto value = glz::object(
+            "ru_utime", &T::ru_utime,
+            "ru_stime", &T::ru_stime,
+            "ru_maxrss", &T::ru_maxrss,
+            "ru_ixrss", &T::ru_ixrss,
+            "ru_idrss", &T::ru_idrss,
+            "ru_isrss", &T::ru_isrss,
+            "ru_minflt", &T::ru_minflt,
+            "ru_majflt", &T::ru_majflt,
+            "ru_nswap", &T::ru_nswap,
+            "ru_inblock", &T::ru_inblock,
+            "ru_oublock", &T::ru_oublock,
+            "ru_msgsnd", &T::ru_msgsnd,
+            "ru_msgrcv", &T::ru_msgrcv,
+            "ru_nsignals", &T::ru_nsignals,
+            "ru_nvcsw", &T::ru_nvcsw,
+            "ru_nivcsw", &T::ru_nivcsw
+    );
+    // clang-format on
+};
--- scripts/RAPL/src/main.cpp
+++ scripts/RAPL/src/main.cpp
@@ -11,18 +11,12 @@
 #include <utility>
 #include <vector>
 
-#include <sys/resource.h>
-#include <sys/time.h>
-
 #include <glaze/core/common.hpp>
 #include <glaze/json/write.hpp>
-namespace glz {
-template <class T>
-struct meta;
-}
 
 #include <rapl/cpu.hpp>
 #include <rapl/rapl.hpp>
+#include <rapl/rusage.hpp>
 
 using Clock = std::chrono::high_resolution_clock;
 
@@ -53,47 +47,6 @@ struct ScopeExit {
     std::function<void()> f;
 };
 
-struct timeval& operator-=(struct timeval& left, const struct timeval& right) {
-    left.tv_sec -= right.tv_sec;
-    left.tv_usec -= right.tv_usec;
-    if (left.tv_usec < 0) {
-        left.tv_sec -= 1;
-        left.tv_usec += 1000000;
-    }
-    return left;
-}
-
-struct rusage& operator-=(struct rusage& left, const struct rusage& right) {
-    left.ru_utime -= right.ru_utime;
-    left.ru_stime -= right.ru_stime;
-    left.ru_maxrss -= right.ru_maxrss;
-    left.ru_ixrss -= right.ru_ixrss;
-    left.ru_idrss -= right.ru_idrss;
-    left.ru_isrss -= right.ru_isrss;
-    left.ru_minflt -= right.ru_minflt;
-    left.ru_majflt -= right.ru_majflt;
-    left.ru_nswap -= right.ru_nswap;
-    left.ru_inblock -= right.ru_inblock;
-    left.ru_oublock -= right.ru_oublock;
-    left.ru_msgsnd -= right.ru_msgsnd;
-    left.ru_msgrcv -= right.ru_msgrcv;
-    left.ru_nsignals -= right.ru_nsignals;
-    left.ru_nvcsw -= right.ru_nvcsw;
-    left.ru_nivcsw -= right.ru_nivcsw;
-
-    return left;
-}
-
-struct timeval operator-(struct timeval left, const struct timeval& right) {
-    left -= right;
-    return left;
-}
-
-struct rusage operator-(struct rusage left, const struct rusage& right) {
-    left -= right;
-    return left;
-}
-
 struct Result {
     Clock::rep runtime;
     rapl::DoubleSample energy;
@@ -119,37 +72,6 @@ struct glz::meta<rapl::DoubleSample> {
         "dram", &T::dram,
 #endif
     });
-    // clang-format on
-};
-
-template <>
-struct glz::meta<struct timeval> {
-    using T = struct timeval;
-    [[maybe_unused]] static constexpr auto value = glz::object("tv_sec", &T::tv_sec, "tv_usec", &T::tv_usec);
-};
-
-template <>
-struct glz::meta<struct rusage> {
-    using T = struct rusage;
-    // clang-format off
-    [[maybe_unused]] static constexpr auto value = glz::object(
-            "ru_utime", &T::ru_utime,
-            "ru_stime", &T::ru_stime,
-            "ru_maxrss", &T::ru_maxrss,
-            "ru_ixrss", &T::ru_ixrss,
-            "ru_idrss", &T::ru_idrss,
-            "ru_isrss", &T::ru_isrss,
-            "ru_minflt", &T::ru_minflt,
-            "ru_majflt", &T::ru_majflt,
-            "ru_nswap", &T::ru_nswap,
-            "ru_inblock", &T::ru_inblock,
-            "ru_oublock", &T::ru_oublock,
-            "ru_msgsnd", &T::ru_msgsnd,
-            "ru_msgrcv", &T::ru_msgrcv,
-            "ru_nsignals", &T::ru_nsignals,
-            "ru_nvcsw", &T::ru_nvcsw,
-            "ru_nivcsw", &T::ru_nivcsw
-    );
     // clang-format on
 };
 
