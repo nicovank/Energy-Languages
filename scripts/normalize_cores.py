db890abe1cff66f6a88011d1aff10427f003087c
Nicolas van Kempen <nvankemp@gmail.com>
2024-06-13 00:35:39 -0400

    Add some work in progress

--- scripts/normalize_cores.py
+++ scripts/normalize_cores.py
@@ -1,8 +1,8 @@
 import argparse
-import math
 import statistics
 
 import matplotlib.pyplot as plt
+import scipy
 
 from . import utils
 
@@ -17,31 +17,110 @@ def main(args: argparse.Namespace) -> None:
 
     for language in args.languages:
         for benchmark in data[language].keys():
-            x = statistics.mean(
-                [
-                    utils.cpu_usage(
-                        utils.timeval_to_seconds(r["rusage"]["ru_utime"]),
-                        utils.timeval_to_seconds(r["rusage"]["ru_stime"]),
-                        1e-3 * r["runtime_ms"],
+            if args.no_mean:
+                for r in data[language][benchmark]:
+                    xs.append(
+                        utils.cpu_usage(
+                            utils.timeval_to_seconds(r["rusage"]["ru_utime"]),
+                            utils.timeval_to_seconds(r["rusage"]["ru_stime"]),
+                            1e-3 * r["runtime_ms"],
+                        )
                     )
-                    for r in data[language][benchmark]
-                ]
-            )
 
-            y = statistics.mean(
-                [
-                    sum([s["energy"]["pkg"] for s in r["energy_samples"]])
-                    / (1e-3 * r["runtime_ms"])
-                    for r in data[language][benchmark]
-                ]
-            )
+                    ys.append(
+                        sum([s["energy"]["pkg"] for s in r["energy_samples"]])
+                        / (1e-3 * r["runtime_ms"])
+                    )
+            else:
+                xs.append(
+                    statistics.mean(
+                        [
+                            utils.cpu_usage(
+                                utils.timeval_to_seconds(r["rusage"]["ru_utime"]),
+                                utils.timeval_to_seconds(r["rusage"]["ru_stime"]),
+                                1e-3 * r["runtime_ms"],
+                            )
+                            for r in data[language][benchmark]
+                        ]
+                    )
+                )
 
-            xs.append(x)
-            ys.append(y)
+                ys.append(
+                    statistics.mean(
+                        [
+                            sum([s["energy"]["pkg"] for s in r["energy_samples"]])
+                            / (1e-3 * r["runtime_ms"])
+                            for r in data[language][benchmark]
+                        ]
+                    )
+                )
+
+    # Group by language:
+    xs_grouped = []
+    ys_grouped = []
+
+    for language in args.languages:
+        xs_grouped.append([])
+        ys_grouped.append([])
+        for benchmark in data[language].keys():
+            if args.no_mean:
+                for r in data[language][benchmark]:
+                    xs_grouped[-1].append(
+                        utils.cpu_usage(
+                            utils.timeval_to_seconds(r["rusage"]["ru_utime"]),
+                            utils.timeval_to_seconds(r["rusage"]["ru_stime"]),
+                            1e-3 * r["runtime_ms"],
+                        )
+                    )
+
+                    ys_grouped[-1].append(
+                        sum([s["energy"]["pkg"] for s in r["energy_samples"]])
+                        / (1e-3 * r["runtime_ms"])
+                    )
+            else:
+                xs_grouped[-1].append(
+                    statistics.mean(
+                        [
+                            utils.cpu_usage(
+                                utils.timeval_to_seconds(r["rusage"]["ru_utime"]),
+                                utils.timeval_to_seconds(r["rusage"]["ru_stime"]),
+                                1e-3 * r["runtime_ms"],
+                            )
+                            for r in data[language][benchmark]
+                        ]
+                    )
+                )
+
+                ys_grouped[-1].append(
+                    statistics.mean(
+                        [
+                            sum([s["energy"]["pkg"] for s in r["energy_samples"]])
+                            / (1e-3 * r["runtime_ms"])
+                            for r in data[language][benchmark]
+                        ]
+                    )
+                )
 
     plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
     with plt.style.context("bmh"):
         plt.scatter(xs, ys)
+        plt.plot(list(range(1, 17)), [
+            34.1687, 46.5196, 50.3137, 52.3241, 54.5677, 56.5167, 58.4932, 60.9066, 63.8334, 66.3835, 68.3035, 70.4256, 73.0678, 75.3255, 77.6687, 79.6539
+        ], color="red", linestyle='dashed',)
+
+        # Plot grouped"
+        # for i, language in enumerate(args.languages):
+        #     plt.scatter(xs_grouped[i], ys_grouped[i], label=language.replace("#", "\\#"))
+        # plt.legend()
+
+        # slope, intercept, _, p_value, _ = scipy.stats.linregress(xs, ys)
+        # plt.plot(
+        #     [min(xs), max(xs)],
+        #     [intercept + slope * min(xs), intercept + slope * max(xs)],
+        #     color="red",
+        # )
+        # print(f"p-value: {p_value}")
+
         plt.xlabel("Average cores used")
         plt.ylabel("Average power [W]")
         plt.ylim(bottom=0)
@@ -69,6 +148,7 @@ if __name__ == "__main__":
             "Lua",
         ],
     )
+    parser.add_argument("--no-mean", action="store_true")
     parser.add_argument("--format", type=str, default="png")
     parser.add_argument("--no-title", action="store_true")
     main(parser.parse_args())
