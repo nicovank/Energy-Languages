a3dd06471585dabe159959c4006d49401d52337d
Nicolas van Kempen <nvankemp@gmail.com>
2024-07-11 15:52:21 -0400

    Remove unused perf counters

--- scripts/RAPL/src/main.cpp
+++ scripts/RAPL/src/main.cpp
@@ -20,12 +20,8 @@
 #include <linux/perf_event.h>
 // clang-format off
 #define RAPL_BENCHMARK_COUNTERS_EVENTS {                                                                               \
-        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES},                                                                \
-        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS},                                                              \
         {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_REFERENCES},                                                          \
         {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES},                                                              \
-        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_INSTRUCTIONS},                                                       \
-        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES},                                                             \
         {PERF_TYPE_SOFTWARE, PERF_COUNT_SW_TASK_CLOCK}                                                                 \
 }
 // clang-format on
