153978712631feb354ab5db7062090e636b6b6dc
Nicolas van Kempen <nvankemp@gmail.com>
2024-07-11 17:09:18 +0000

    Version 2 of time-limiter script

--- scripts/RAPL/src/time-limiter.cpp
+++ scripts/RAPL/src/time-limiter.cpp
@@ -1,68 +1,50 @@
+#include <cstdio>
 #include <cstdlib>
-#include <iostream>
-#include <vector>
 
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-std::vector<pid_t> get_child_processes(pid_t pid) {
-    std::vector<pid_t> children;
+#include <fmt/core.h>
 
-    auto* dir = opendir("/proc");
+void kill_process_tree(pid_t pid) {
+    auto* dir = opendir(fmt::format("/proc/{}/task", pid).c_str());
     if (!dir) {
-        std::cerr << "Failed to open /proc directory." << std::endl;
-        return {};
+        fmt::println(stderr, "Failed to open /proc directory.");
+        return;
     }
 
     struct dirent* entry;
     while ((entry = readdir(dir)) != nullptr) {
         if (entry->d_type == DT_DIR) {
-            const auto child_pid = pid_t(std::atoi(entry->d_name));
-            if (child_pid <= 0 || child_pid == pid) {
+            const auto tid = std::atoi(entry->d_name);
+            if (tid <= 0 || tid == pid) {
                 continue;
             }
 
-            const auto path = std::string("/proc/") + entry->d_name + "/stat";
+            const auto path = fmt::format("/proc/{}/task/{}", pid, tid);
             if (auto* file = fopen(path.c_str(), "r")) {
-                pid_t ppid;
-                if (fscanf(file, "%*d %*s %*c %d", &ppid) != 1) {
-                    fclose(file);
-                    continue;
+                pid_t child_pid;
+                while (fscanf(file, "%d", &child_pid) != EOF) {
+                    kill_process_tree(child_pid);
+                    kill(child_pid, SIGKILL);
                 }
-                fclose(file);
 
-                if (ppid == pid) {
-                    children.push_back(child_pid);
-                    const auto subchildren = get_child_processes(child_pid);
-                    children.insert(children.end(), subchildren.begin(), subchildren.end());
-                }
+                fclose(file);
             }
         }
     }
 
     closedir(dir);
 
-    return children;
-}
-
-void kill_process_tree(pid_t pid) {
-    std::vector<pid_t> children = get_child_processes(pid);
-
-    // Kill all child processes.
-    for (pid_t child_pid : children) {
-        kill(child_pid, SIGKILL);
-    }
-
-    // Kill the original process.
     kill(pid, SIGKILL);
 }
 
 int main(int argc, char** argv) {
     // First argument is delay in seconds, remaining arguments are the command to run.
     if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <delay> <command...>" << std::endl;
+        fmt::println(stderr, "Usage: {} <delay> <command...>", argv[0]);
         return EXIT_FAILURE;
     }
 
@@ -70,13 +52,13 @@ int main(int argc, char** argv) {
 
     const pid_t pid = fork();
     if (pid == -1) {
-        std::cerr << "Failed to fork." << std::endl;
+        fmt::println(stderr, "Failed to fork.");
         return EXIT_FAILURE;
     }
 
     if (pid == 0) {
         execvp(argv[2], argv + 2);
-        std::cerr << "Failed to execute command." << std::endl;
+        fmt::println(stderr, "Failed to execute command.");
         return EXIT_FAILURE;
     }
 
@@ -85,7 +67,7 @@ int main(int argc, char** argv) {
     // If process has already exited after delay, return failure.
     int status;
     if (waitpid(pid, &status, WNOHANG) != 0) {
-        std::cerr << "Process exited before timeout." << std::endl;
+        fmt::println(stderr, "Process exited before timeout.");
         return EXIT_FAILURE;
     }
 
@@ -93,7 +75,7 @@ int main(int argc, char** argv) {
     kill_process_tree(pid);
     waitpid(pid, &status, 0);
     if (!WIFSIGNALED(status) || WTERMSIG(status) != SIGKILL) {
-        std::cerr << "Failed to kill process." << std::endl;
+        fmt::println(stderr, "Failed to kill process.");
         return EXIT_FAILURE;
     }
 
