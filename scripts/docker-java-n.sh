f58ce8bfec57bd4be6bd71d389df42799dc6a8a6
Nicolas van Kempen <nvankemp@gmail.com>
2024-09-26 13:04:23 -0400

    Update docker-java-n.sh

--- scripts/docker-java-n.sh
+++ scripts/docker-java-n.sh
@@ -2,6 +2,12 @@
 
 set -euxo pipefail
 
+if [ $# -lt 2 ]; then
+    echo "Usage: sudo docker-java-n.sh <LANGUAGE> <DATA_OUTPUT_DIR>"
+    echo "Example: sudo docker-java-n.sh Java docker-default"
+    exit 1
+fi
+
 if [ ! -d "experiments/$1-N" ]; then
   echo "experiments/$1-N does exist."
   exit 1
@@ -9,10 +15,10 @@ fi
 
 for i in {1..20}
 do
-    docker run -it --rm --privileged -e NNNNN=$i -v `pwd`/data/`hostname -s`/docker-default:/root/data energy-languages \
+    docker run -it --rm --privileged -e NNNNN=$i -v `pwd`/data/`hostname -s`/$2:/root/data energy-languages \
         --benchmark-root experiments \
         --languages $1-N \
         --warmup 1 \
         --iterations 5
-    mv "$(pwd)/data/$(hostname -s)/docker-default/$1-N" "$(pwd)/data/$(hostname -s)/docker-default/$1-$i"
+    mv "$(pwd)/data/$(hostname -s)/$2/$1-N" "$(pwd)/data/$(hostname -s)/$2/$1-$i"
 done
